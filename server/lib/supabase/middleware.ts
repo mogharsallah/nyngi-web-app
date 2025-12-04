d485298119813d737fb2bb139aa8e00678fcbc25
Mouhamed GHARSALLAH <gmedhoover@gmail.com>
2025-12-04 15:40:18 +0100

    fix: improve user session validation logic in middleware

--- server/lib/supabase/middleware.ts
+++ server/lib/supabase/middleware.ts
@@ -38,7 +38,11 @@ export async function updateSession(request: NextRequest) {
 
   const user = data?.claims;
 
-  if (!user && !request.nextUrl.pathname.startsWith("/auth")) {
+  if (
+    !user &&
+    !request.nextUrl.pathname.startsWith("/auth") &&
+    request.nextUrl.pathname !== "/"
+  ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
     url.pathname = "/auth";
